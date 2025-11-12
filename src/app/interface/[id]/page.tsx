87dac9e173877ead7e7ca0a0c75db3ac1e009788
rodrigo brinate <rbp20199@gmail.com>
2025-11-12 10:34:46 -0300

    teste

--- src/app/interface/[id]/page.tsx
+++ src/app/interface/[id]/page.tsx
@@ -281,6 +281,7 @@ export default function InterfaceDetailPage() {
             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
               Data/Hora Final
             </label>
+            
             <DatePicker
               selected={endDate}
               onChange={(date: any) => setEndDate(date)}
